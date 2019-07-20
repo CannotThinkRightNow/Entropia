8f25cff24101639cab8f568d06464ddddb7c2e32
William So <williamso26144225@gmail.com>
2019-07-20 15:24:24 +0800

    Include 'cmake/variables.cmake' after 'project()'

--- cmake/variables.cmake
+++ cmake/variables.cmake
@@ -7,9 +7,6 @@ set(SECTION_HEADER "==================================================")
 set(SECTION_SPLITTER "--------------------------------------------------")
 set(SECTION_FOOTER "==================================================")
 
-# Project arguments
-get_arguments(PROJECT_ARGS TRUE)
-
 # Environment variables
 if (NOT DEFINED ENV{CI})
   set(ENV{CI} FALSE)
@@ -97,7 +94,7 @@ endif ()
 
 if ($ENV{CI})
   set(CMAKE_BUILD_TYPE $ENV{CONFIG})
-  list(APPEND PROJECT_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}")
+  set(PROJECT_ARGS ${PROJECT_ARGS} "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}")
 endif ()
 
 # Boost
