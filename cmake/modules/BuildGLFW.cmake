dead5b4b50b2b9b8a9d3d2943e7cf9393980bc65
William So <williamso26144225@gmail.com>
2019-07-20 10:01:16 +0800

    Set no-dev options in modules instead of 'invoke.cmake'

--- cmake/modules/BuildGLFW.cmake
+++ cmake/modules/BuildGLFW.cmake
@@ -19,11 +19,11 @@ string(REPLACE "\"" "" GLFW_DEFAULT_BRANCH ${GLFW_DEFAULT_BRANCH})
 
 set(EXTERNALPROJECT_BUILD_NAME "glfw")
 set(EXTERNALPROJECT_BUILD_PREFIX "${PROJECT_BINARY_DIR}/external/${EXTERNALPROJECT_BUILD_NAME}")
+set(EXTERNALPROJECT_BUILD_CONFIG_ARGS "-Wno-dev -Wno-error=dev -Wno-error=deprecated")
 set(EXTERNALPROJECT_BUILD_ARGS ${EXTERNALPROJECT_BUILD_NAME}_project GIT_REPOSITORY "https://github.com/glfw/glfw.git"
                                                                      GIT_TAG "origin/${GLFW_DEFAULT_BRANCH}"
                                                                      PREFIX "${EXTERNALPROJECT_BUILD_PREFIX}"
                                                                      CMAKE_ARGS -G "${CMAKE_GENERATOR}" ${PROJECT_ARGS} "-DCMAKE_INSTALL_PREFIX:PATH=${CONTRIB_DIR}" "-DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}" "-DGLFW_BUILD_EXAMPLES:BOOL=${GLFW_BUILD_EXAMPLES}" "-DGLFW_BUILD_TESTS:BOOL=${GLFW_BUILD_TESTS}" "-DGLFW_BUILD_DOCS:BOOL=${GLFW_BUILD_DOCS}" "-DGLFW_INSTALL:BOOL=${GLFW_INSTALL}" "-DGLFW_VULKAN_STATIC:BOOL=${GLFW_VULKAN_STATIC}"
                                                                      GIT_SHALLOW TRUE
                                                                      GIT_PROGRESS TRUE)
-set(EXTERNALPROJECT_BUILD_NO_DEV TRUE)
 include("${PROJECT_SOURCE_DIR}/cmake/ExternalProject_Build/invoke.cmake")
